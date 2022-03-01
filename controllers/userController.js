79d0249c908a1a8cab8bc617638acbcb5be7f837
Elvan H <elvanhd@gmail.com>
2022-03-01 15:20:01 +0700

    fungsi untuk login dan membuat jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,13 @@
-const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 exports.registerUser = asyncHandler(async (req, res) => {
   // Destructure the user object from the request body
@@ -31,11 +37,18 @@ exports.registerUser = asyncHandler(async (req, res) => {
     password: hashedPassword,
   });
 
+  const user = {
+    id: newUser._id,
+    name: newUser.name,
+    email: newUser.email,
+    token: generateToken(newUser._id),
+  };
+
   if (newUser) {
     // Send response
     res.status(201).json({
       message: 'User registered successfully',
-      user: newUser,
+      user: user,
     });
   } else {
     // Failed to register user
@@ -45,13 +58,51 @@ exports.registerUser = asyncHandler(async (req, res) => {
 });
 
 exports.loginUser = asyncHandler(async (req, res) => {
+  // Destructure the user object from the request body
+  const { email, password } = req.body;
+
+  // Basic validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('Please provide all required fields');
+  }
+
+  // Check for existing user
+  const existingUser = await User.findOne({ email: email });
+  if (!existingUser) {
+    res.status(400);
+    throw new Error('User does not exist');
+  }
+
+  // Check password
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+  if (!isMatch) {
+    res.status(400);
+    throw new Error('Incorrect credentials');
+  }
+
+  const user = {
+    _id: existingUser._id,
+    name: existingUser.name,
+    email: existingUser.email,
+    token: generateToken(existingUser._id),
+  };
+
+  // Send response
   res.json({
     message: 'User logged in successfully',
+    user: user,
   });
 });
 
 exports.currentUser = (req, res) => {
   res.json({
     message: 'Current user',
+  });
+};
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: '30d',
   });
 };
